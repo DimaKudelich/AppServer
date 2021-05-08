e460b66735b9670709dcd42885547eac41694909
Kudelich Dmitry <dima.kudelich2@mail.ru>
2021-05-08 23:46:32 +0300

    Adding hibernate and file hibernate.properties, and adding interaction between hibernate and database config

--- src/main/java/com/kudelich/server/config/DatabaseConfig.java
+++ src/main/java/com/kudelich/server/config/DatabaseConfig.java
@@ -1,13 +1,21 @@
 package com.kudelich.server.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories("com.kudelich.server.repository")
@@ -17,4 +25,41 @@ import javax.annotation.Resource;
 public class DatabaseConfig {
     @Resource
     private Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
+
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    private Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream stream = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(stream);
+
+            return properties; 
+        }catch (IOException ex){
+            throw new IllegalArgumentException("Can't find hibernate/properties",ex);
+        }
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
 }
